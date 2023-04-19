d24435d480f3a34ebba2c3f1a26f0b4093cdf54d
jeonyoungho <yhjun1000@gmail.com>
2023-04-19 22:28:12 +0900

    코드 정리

--- src/test/java/com/example/footballreservationscheduler/core/EmailServiceTest.java
+++ src/test/java/com/example/footballreservationscheduler/core/EmailServiceTest.java
@@ -4,9 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.io.PrintWriter;
-import java.io.StringWriter;
-
 @SpringBootTest
 class EmailServiceTest {
 
