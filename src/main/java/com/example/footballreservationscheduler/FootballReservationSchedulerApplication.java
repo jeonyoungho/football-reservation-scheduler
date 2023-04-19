df315e60fe8bfa1e522620f9a854fe7ddd0a7746
jeonyoungho <yhjun1000@gmail.com>
2023-04-19 16:49:39 +0900

    initialize project

---
+++ src/main/java/com/example/footballreservationscheduler/FootballReservationSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.example.footballreservationscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballReservationSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballReservationSchedulerApplication.class, args);
+	}
+
+}
